bb49b8adc23147afc1fe4951045e0af97c274ec3
jass <nanarhjaspreet@gmail.com>
2024-10-02 17:55:12 -0400

    jjj

--- examples/first-docker-file/app.py
+++ examples/first-docker-file/app.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    name = os.getenv('NAME', 'World')  # Get the 'NAME' environment variable or default to 'World'
+    return f"Hello {name}!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
+
